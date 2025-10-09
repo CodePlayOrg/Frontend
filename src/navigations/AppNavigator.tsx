691ef1cdd384edee63ff4f13a2ce5690db59fc47
Codeplay_Nawon <100nawon@naver.com>
2025-10-10 02:06:17 +0900

    로그인, 회원가입 완성

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createStackNavigator } from '@react-navigation/stack';
 import StartScreen from '../screens/StartScreen';
-import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 export type RootStackParamList = {
   Start: undefined;
-  Login: undefined;
   Signup: undefined;
+  Login: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
-const AppNavigator: React.FC = () => {
+const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={StartScreen} options={{ title: '시작 화면' }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: '로그인' }} />
-        <Stack.Screen name="Signup" component={SignupScreen} options={{ title: '회원가입' }} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
